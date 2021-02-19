b561457da8ce0ae94827495509a93718813c564d
Leandro Dourado <leandrotux@hotmail.com>
2021-02-19 11:39:58 -0300

    Add AlertDialog and functions

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,35 +6,62 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _listaTarefas = ["Ir ao mercado", "Estudar", "Ir para academia"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Lista de Tarefas"),
-      ),
-      body: Text("Conteudo"),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.purple,
-        foregroundColor: Colors.white,
-        elevation: 6,
-        //mini: true,
-        child: Icon(Icons.add),
-        onPressed: () {
-          print("Resultado: botão clicado!");
-        },
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        child: Row(
+        appBar: AppBar(
+          title: Text("Lista de Tarefas"),
+          backgroundColor: Colors.purple,
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.purple,
+          foregroundColor: Colors.white,
+          elevation: 6,
+          //mini: true,
+          child: Icon(Icons.add),
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text("Adicionar Tarefas"),
+                    content: TextField(
+                      decoration:
+                          InputDecoration(labelText: "Digite sua tarefa"),
+                      onChanged: (text) {},
+                    ),
+                    actions: [
+                      FlatButton(
+                        child: Text("Cancelar"),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                      FlatButton(
+                        child: Text("Adicionar"),
+                        onPressed: () {
+                          //salvar
+                          Navigator.pop(context);
+                        },
+                      )
+                    ],
+                  );
+                });
+          },
+        ),
+        body: Column(
           children: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.add),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _listaTarefas.length,
+                itemBuilder: (contex, index) {
+                  return ListTile(
+                    title: Text(_listaTarefas[index]),
+                  );
+                },
+              ),
             )
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
