f98017feb4d26bca791f3b6c69b30c4b066204b5
Leandro Alves Dourado Arcanjo <leandrotux@hotmail.com>
2021-04-19 22:04:56 -0300

    Fix home

--- lib/Home.dart
+++ lib/Home.dart
@@ -10,76 +10,45 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List _listaTarefas = [];
-
-  _saveData() async {
-    final diretorio = await getApplicationDocumentsDirectory();
-    var arquivo = File("${diretorio.path}/dados.json");
-
-    Map<String, dynamic> tarefa = Map();
-    tarefa["titulo"] = "Ir ao mercado";
-    tarefa["realizada"] = false;
-    _listaTarefas.add(tarefa);
-
-    String dados = json.encode(_listaTarefas);
-    arquivo.writeAsString(dados);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Lista de Tarefas"),
-          backgroundColor: Colors.purple,
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.purple,
-          foregroundColor: Colors.white,
-          elevation: 6,
-          //mini: true,
-          child: Icon(Icons.add),
-          onPressed: () {
-            showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: Text("Adicionar Tarefas"),
-                    content: TextField(
-                      decoration:
-                          InputDecoration(labelText: "Digite sua tarefa"),
-                      onChanged: (text) {},
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.purple,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purple,
+        foregroundColor: Colors.white,
+        elevation: 6,
+        child: Icon(Icons.add),
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Adicionar tarefa"),
+                  content: TextField(
+                    decoration: InputDecoration(labelText: "Digite sua tarefa"),
+                    onChanged: (text) {},
+                  ),
+                  actions: [
+                    FlatButton(
+                      child: Text("Cancelar"),
+                      onPressed: () => Navigator.pop(context),
                     ),
-                    actions: [
-                      FlatButton(
-                        child: Text("Cancelar"),
-                        onPressed: () => Navigator.pop(context),
-                      ),
-                      FlatButton(
-                        child: Text("Adicionar"),
-                        onPressed: () {
-                          //salvar
-                          Navigator.pop(context);
-                        },
-                      )
-                    ],
-                  );
-                });
-          },
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: _listaTarefas.length,
-                itemBuilder: (contex, index) {
-                  return ListTile(
-                    title: Text(_listaTarefas[index]),
-                  );
-                },
-              ),
-            )
-          ],
-        ));
+                    FlatButton(
+                      child: Text("Adicionar"),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    )
+                  ],
+                );
+              });
+        },
+      ),
+    );
   }
 }
